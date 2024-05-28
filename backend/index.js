fd6eb29c860688f7a1baf20b2defbf01e3d79539
Phạm Vũ Bão <vubao201@gmail.com>
2024-05-28 12:29:15 +0700

    connect to database, add .env and .gitignore files

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = 8000;
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+require("./db");
 
 app.use(bodyParser.json());
 const allowedOrigins = ["http://localhost:3000"]; // Add more origins as needed

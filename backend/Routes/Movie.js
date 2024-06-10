c98cde2cde7ae2a403c82d16ef0b708b2a4ef4c6
Phạm Vũ Bão <vubao201@gmail.com>
2024-06-10 15:33:25 +0700

    Create Booking, Movie, Screen Schemas

--- backend/Routes/Movie.js
+++ backend/Routes/Movie.js
@@ -1,2 +1,7 @@
 const express = require("express");
 const router = express.Router();
+
+const User = require("../Models/UserSchema");
+const Movie = require("../Models/MovieSchema");
+const Booking = require("../Models/BookingSchema");
+const Screen = require("../Models/ScreenSchema");

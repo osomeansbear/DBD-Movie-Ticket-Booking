3a6d1f2e6b99f0404f64fa8c256ea0d6596c23d3
Phạm Vũ Bão <vubao201@gmail.com>
2024-06-10 19:18:42 +0700

    Update Movie.js

--- backend/Routes/Movie.js
+++ backend/Routes/Movie.js
@@ -5,3 +5,12 @@ const User = require("../Models/UserSchema");
 const Movie = require("../Models/MovieSchema");
 const Booking = require("../Models/BookingSchema");
 const Screen = require("../Models/ScreenSchema");
+
+router.get("/test", async (req, res) => {
+  res.json({
+    message: "Movie API is working",
+  });
+});
+
+router.use(errorHandler);
+module.exports = router;
